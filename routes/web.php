f40188c14bd86d7d7c6a712d6009504bdb811a9f
dcastanho33 <dcastanho33@gmail.com>
2021-08-14 17:10:18 -0300

    testando rotas

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,84 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/ola', function (){
+    echo "Olá";
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/ola/{nome}/{sobrenome}', function ($nome, $sobrenome){
+    echo "Olá, seja bem-vindo $nome $sobrenome!";
+});
+
+Route::get('/seunome/{nome?}', function ($nome=null){
+    if (isset($nome))
+        echo "Olá, seja bem-vindo, $nome!";
+    else 
+        echo "Você não digitou um nome";
+});
+
+Route::get('/rotacomregras/{nome}/{n}', function ($nome, $n){
+    for($i=0;$i<$n;$i++)
+        echo "Olá, seja bem-vindo $nome!<br>";
+})->where('nome', '[A-Za-z]+')
+  ->where('n', '[0-9]+');
+
+Route::prefix('/application')->group(function(){
+    
+    Route::get('/', function(){
+        return view ('app');
+    })->name('app');
+ 
+    Route::get('/user', function(){
+        return view ('user');
+    })->name('app.user');
+ 
+    Route::get('/profile', function(){
+        return view ('profile');
+    })->name('app.profile');
+});
+
+Route::get('/produtos', function(){
+    echo "<h1>Produtos</h1>";
+    echo "<ol>";
+        echo "<li>Notebook </li>";
+        echo "<li>Mouse </li>";
+        echo "<li>Impressora </li>";
+    echo "</ol>";        
+})->name('meusprodutos');
+
+Route::redirect('todosprodutos', 'produtos', 301); //redirecionamento de todosprodutos para produtos
+
+Route::get('todosprodutos2', function(){
+    return redirect()->route('meusprodutos');
+});
+
+//////////////////////////////////
+
+Route::post('/requisicoes', function(Request $request){ 
+    return 'Hello POST';
+});
+
+Route::delete('/requisicoes', function(Request $request){ 
+    return 'Hello DELETE';
+});
+
+Route::put('/requisicoes', function(Request $request){ 
+    return 'Hello PUT';
+});
+
+Route::patch('/requisicoes', function(Request $request){ 
+    return 'Hello PATCH';
+});
+
+
+Route::options('/requisicoes', function(Request $request){ 
+    return 'Hello OPTIONS';
+});
+
+
